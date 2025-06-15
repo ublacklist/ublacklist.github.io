b69ad662a10952854b9d495739eb2d1539b90d98
iorate <682043+iorate@users.noreply.github.com>
2025-06-15 21:45:32 +0900

    disable Rspack persitent cache for now
    Ref: https://github.com/web-infra-dev/rspack/issues/10599

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -84,7 +84,17 @@ const config: Config = {
   } satisfies Preset.ThemeConfig,
 
   future: {
-    experimental_faster: true,
+    experimental_faster: {
+      swcJsLoader: true,
+      swcJsMinimizer: true,
+      swcHtmlMinimizer: true,
+      lightningCssMinimizer: true,
+      mdxCrossCompilerCache: true,
+      rspackBundler: true,
+      // Disable Rspack persistent cache for now
+      rspackPersistentCache: false,
+      ssgWorkerThreads: true,
+    },
     v4: true,
   },
 };
